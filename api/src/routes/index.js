ae4224474ad782860d8eec53b59db6d5add415a5
Thomasereno <thomasereno1@gmail.com>
2022-01-31 12:18:33 -0300

    proyecto individual actualizado

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -22,7 +22,10 @@ const getBreedsInfo = async () => {
       height: el.height.metric,
       weight: el.weight.metric,
       lifeSpan: el.life_span,
-      temperament: el.temperament,
+      temperament: [el.temperament]
+        .join()
+        .split(",")
+        .map((el) => el.trim()),
       image: el.image.url,
     };
   });
@@ -50,14 +53,125 @@ const getAllBreeds = async () => {
 };
 
 router.get("/dogs", async (req, res) => {
-    const name = req.query.name
-    let totalBreeds = await getAllBreeds()
-    if(name){
-        let dogBreed = await totalBreeds.filter(el => el.name.toLowerCase().includes(name.toLowerCase()))
-        dogBreed.length ? res.status(200).send(dogBreed) : res.status(404).send("404 Breed Not Found :(")
-    } else {
-        res.status(200).send(totalBreeds)
+  const name = req.query.name;
+  let totalBreeds = await getAllBreeds();
+  if (name) {
+    let dogBreed = await totalBreeds.filter((el) =>
+      el.name.toLowerCase().includes(name.toLowerCase())
+    );
+    dogBreed.length
+      ? res.status(200).send(dogBreed)
+      : res.status(404).send("404 Breed Not Found :(");
+  } else {
+    res.status(200).send(totalBreeds);
+  }
+});
+
+// A TRABAJAR. ESTO ES LO DE TEMPERAMENTS QUE ES TODO UNA STRING DE MIERDA
+
+// router.get("/temperaments", async (req, res) => {
+//     const temperamentsApi = await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`)
+//     const temperaments = temperamentsApi.data.map( el => el.temperament)
+//     const tempEach = temperaments.map(el => {
+//         for (let i = 0; i < el.length; i++) return el[i]})
+//         console.log(tempEach)
+//     tempEach.forEach(el => {
+//         Temperament.findOrCreate({
+//             where: { temperament: el }
+//         })
+//     })
+
+//     const allTemperaments = await Temperament.findAll()
+//     res.send(allTemperaments)
+// })
+
+// router.get("/temperaments", async (req, res) => {
+//     const temperamentsApi = await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`)
+//     const temperaments = temperamentsApi.data.join.split(',').sort( el => el.temperament )
+//     const temperamentsEach = temperaments.sort(el => el)
+//     console.log(temperamentsEach)
+//     temperamentsEach.sort( el => {
+//         if(el !== '') {
+//         Temperament.findOrCreate({
+//         where: { name: el }
+//      })
+//    }
+// })
+//     const allTemperaments = await Temperament.findAll()
+//     res.send(allTemperaments)
+// })
+
+// AYUDA DE STACK OVERFLOW
+
+// router.get("/temperaments", async (req, res) => {
+//   let result = []
+//   const temperamentsApi = await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`)
+//   const temperaments = temperamentsApi.data.forEach( el => el.temperament)
+//   const tempEach = temperaments.forEach(datum=>{
+//     result.push(datum.temperaments);
+// });
+// console.log(result);
+//   tempEach.forEach(el => {
+//       Temperament.findOrCreate({
+//           where: { temperament: el }
+//       })
+//   })
+
+//   const allTemperaments = await Temperament.findAll()
+//   res.send(allTemperaments)
+// })
+
+//AYUDA v2
+
+router.get("/temperaments", async (req, res) => {
+  dogsApi = await getAllBreeds();
+  const dogsDb = dogsApi
+    .map((el) => el.temperament)
+    .join()
+    .split(",");
+  const dogsDbTrim = dogsDb.map((el) => el.trim());
+
+  dogsDbTrim.forEach((el) => {
+    if (el !== "") {
+      Temperament.findOrCreate({
+        where: {
+          temperament: el,
+        },
+      });
     }
-})
+  });
+  res.send("Temperamento creado")
+
+});
+
+// POST DE TEMPERAMENTS
+
+// router.post("/dogs", async (req, res) => {
+//     let {
+//         name,
+//         height,
+//         weight,
+//         lifeSpan,
+//         temperament,
+//         image,
+//         createdInDb,
+//     } = req.body
+
+//     let dogCreated = await Dog.create ({
+//         name,
+//         height,
+//         weight,
+//         lifeSpan,
+//         temperament,
+//         image,
+//         createdInDb
+//     })
+
+//     let temperamentDb = await Temperament.findAll({
+//         where: { temperament : temperament }
+//     })
+//     dogCreated.addTemperament(temperamentDb)
+//     res.send("Perrito creado con exito")
+// })
 
 module.exports = router;
